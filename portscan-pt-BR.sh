ec558d66e109b8cb950671ebf8ca1ad52801136d
Lenny <49320423+lennymouzehine@users.noreply.github.com>
2021-03-19 20:27:02 -0300

    Update portscan-pt-BR.sh

--- portscan-pt-BR.sh
+++ portscan-pt-BR.sh
@@ -4,98 +4,151 @@
 
 
 : '
-	-- + exemplos de uso + --
+	-- + EXEMPLOS DE USO + --
 #Iniciando	    		#Dominio 	#Porta
-chmod +x portscan-pt-BR.sh
-./portscan-pt-BR.sh		127.0.0.1 	1  50
-source portscan-pt-BR.sh	127.0.0.1 	1  50
-bash portscan-pt-BR.sh		192.168.0.1 	80 443
-bash portscan-pt-BR.sh		192.168.0.1 	100 1500
+chmod +x portscan.sh
+./portscan.sh			127.0.0.1 	1  50
+. portscan.sh                   127.0.0.1       1  100
+source portscan.sh		127.0.0.1 	1  50
+bash portscan.sh		192.168.0.1 	80 443
 '
 
 #
-#
-#
-#
+#       PortScan - Lenny Mouzehine
 #
 
-#código
-
+#Cores
 cinza="\033[30;1m"
 rosa="\033[31;1m"
 verde="\033[32;1m"
 laranja="\033[33;1m"
 branco="\033[35;1m"
 
-#dominio() {
-        if [ "$1" == "" ]; then
-                echo "Necessario parametro de dominio"; 
-        else
-                echo -ne "------------------------- $verde[$branco+$verde]$branco\033[m\n"
-                echo -ne $branco"Dominio: $1\033[m\n"
-        fi
-#}
+#função de ajuda
+ajuda() {
+        echo "Seguindo o exemplo de uso:"
+        echo "./portscan-pt-BR.sh"
+        echo "O script fará uma varredura no domínio e portas citadas"
+        echo "É importante que o primeiro parâmetro de porta seja maior que o último parâmetro de porta citado"
+        echo "Este script é recomendado para varreduras internas, pode demorar algum tempo ao tentar usá-lo em redes externas"
+        echo "Digite -v ou --versao para saber informações sobre a versão atual"
+        exit 0
+}
 
-#porta() {
-        if [[ "$2" != "" && "$3" != "" ]]; then
-                echo -ne $branco"Porta: $2 a $3\033[m\n"
-                echo -ne "------------------------- $verde[$branco+$verde]$branco\033[m\n"
-        else
-                echo "Necessario parametros de portas"
-        fi
-#}
+#função de versão
+versao() {
+        echo "PortScan - Versão 1.4 (PT-BR)"
+        echo "Adicionado nova interface"
+        exit 1
+}
+
+#ativador de função
+if test "$1" = "-a"; then
+ajuda
+elif test "$1" = "--ajuda"; then
+ajuda
+elif test "$1" = "-v"; then
+versao
+elif test "$1" = "--versao"; then
+versao
+fi
+
+#target
+read -p "Digite o domínio alvo: " dominio
+read -p "Digite a porta destino inicial: " portainicial
+read -p "Digite a porta destino final: " portafinal
+read -p "Todas as informações estão corretas? [S/n]: " -e -n 1 op
+
+
+
+case $op in
+        (s|S)
+                for carregando in `seq 2`; do
+                echo -n "###"
+                sleep 1
+                done
+                echo -e " 100%"
+                sleep 0
+        ;;
+        (n|N)
+                echo "Retornando..."
+                exit 0;
+        ;;
+                *) echo "Opção inválida"; exit 1;;
+esac
+
+if [ "$dominio" != "" ] && [ "$portainicial" != "" ] && [ "$portafinal" != "" ]; then
+        echo -ne "------------------------- $verde[$branco+$verde]$branco\033[m\n"
+        echo -ne $branco"Domínio: $dominio\033[m\n"
+        echo -ne $branco"Porta: $portainicial até $portafinal\033[m\n"
+        echo -ne "------------------------- $verde[$branco+$verde]$branco\033[m\n"
+elif [ "$dominio" == "-a" ]; then
+ajuda
+else
+        echo -e "\nParâmetros requeridos";
+        echo "Digite -a ou --ajuda para mais informações"
+        exit 0
+fi
 
 function tcp() {
+    dominio=$1
+    portainicial=$2
+    portafinal=$3
     echo "Protocolo TCP selecionado"
     echo ""
     protocolo='tcp'
-    for porta in `seq $2 $3`;
+    for porta in `seq $portainicial $portafinal`;
     do
-    timeout 0 bash -c "</dev/$protocolo/$1/$porta" &>/dev/null \
+    timeout 1 bash -c "</dev/$protocolo/$dominio/$porta" &>/dev/null \
     && echo -ne "+ - -  -- [ PORTA $porta  - ABERTA ] --  - - +     \n" \
     || sleep 0 ; echo -ne "Escaneando porta $porta\e[K\r"
     done
 }
 
-function udp() {
-    echo "Protocolo UDP selecionado"
-    echo ""
-    protocolo='udp'
-    for porta in `seq $2 $3`;
-    do
-    timeout 0 bash -c "</dev/$protocolo/$1/$porta" &>/dev/null \
-    && echo -ne "+ - -  -- [ PORTA $porta  - ABERTA ] --  - - +     \n" \
-    || sleep 0 ; echo -ne "Escaneando porta $porta\e[K\r"
-    done
-}
+#function udp() {
+#    domain=$1
+#    firstport=$2 
+#    finalport=$3 
+#    echo "UDP protocol selected"
+#    echo ""
+#    protocol='udp'
+#    for port in `seq $firstport $finalport`;
+#    do
+#    timeout 1 bash -c "</dev/$protocol/$domain/$port" &>/dev/null \
+#    && echo -ne "+ - -  -- [ PORT $port  - OPEN ] --  - - +     \n" \
+#    || sleep 0 ; echo -ne "Scanning port $port\e[K\r"
+#    done
+#}
 
 opcoes=("tcp" "udp" "sair")
 
-if [[ -z "$1" || -z "$2" || -z "$3" ]]; then
-        #dominio
-        #porta
-        echo "Exemplo de uso: ./portscan-pt-BR.sh google.com 80 81"
-elif (expr $2 + 1 > /dev/null 2>/dev/null) && (expr $3 + 1 > /dev/null 2> /dev/null); then
-                echo -e "Selecione uma das opcoes a seguir\n"
-        select opcs in ${opcoes[@]}
+if [[ -z "$dominio" || -z "$portainicial" || -z "$portafinal" ]]; then
+exit 0
+elif (expr $portainicial + 666 > /dev/null 2>/dev/null) \
+&& (expr $portafinal + 666 > /dev/null 2> /dev/null) \
+&& (("$portainicial" < "$portafinal")); then
+echo -e "Selecione um número entre as seguintes opções\n"
+        select opc in ${opcoes[@]}
         do
-        case $opcs in
+        case $opc in
                 "tcp")
-                        tcp $1 $2 $3
+                        tcp $dominio $portainicial $portafinal
                         break
                 ;;
                 "udp")
-                        udp $1 $2 $3
+#                        udp $domain $firstport $finalport
+                        echo "Indisponivel"
                         break
                 ;;  
                 "sair")
                         echo "Saindo..."
                         break
                 ;;
-                *) echo "Opcao invalida"
+                *) echo "Opção inválida"
         esac
         done
 else
-        echo "AVISO: a porta final nao pode ser menor que a porta inicial"
-        echo "AVISO: parametro porta aceita somente numeros inteiros"
+        echo "AVISO: OS PARÂMETROS DE PORTAS SÓ ACEITAM NÚMEROS INTEIROS"
+        echo -e "AVISO: A PORTA FINAL NÃO PODE SER MENOR QUE A PORTA INICIAL\n"
+        echo -e "Digite -a ou --ajuda para mais informações\n"
 fi
